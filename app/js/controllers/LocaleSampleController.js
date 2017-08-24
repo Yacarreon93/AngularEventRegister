4fedb48a51be09f12ec1b5cfd067193f38569e3e
Yasser Arturo Carreon Lopez <yasser carreon@tiempodev.local>
2017-08-24 16:50:04 -0500

    Include $exceptionHandler

--- app/js/controllers/LocaleSampleController.js
+++ app/js/controllers/LocaleSampleController.js
@@ -5,4 +5,6 @@ eventsApp.controller('LocaleSampleController', function ($scope, $locale) {
   $scope.myDate = Date.now()
   $scope.myFormat = $locale.DATETIME_FORMATS.fullDate
 
+  throw { message: 'error message' }
+
 })
