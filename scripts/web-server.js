8b8bf01b63827b1cdfcddc8f8aab80b8246ed8cf
Yasser Arturo Carreon Lopez <yasser carreon@tiempodev.local>
2017-08-24 11:14:03 -0500

    Include GET/POST services to server

--- scripts/web-server.js
+++ scripts/web-server.js
@@ -1,10 +1,18 @@
 var express = require('express')
 var path = require('path')
+var bodyParser = require('body-parser')
+
+var events = require('./eventsController')
 
 var app = express()
 var rootPath = path.normalize(__dirname + '/../')
 
+app.use(bodyParser.urlencoded({ extended: true }))
+app.use(bodyParser.json())
 app.use(express.static(rootPath + '/app'))
+
+app.get('/data/event/:id', events.get)
+app.post('/data/event/:id', events.save)
 
 app.listen(8000)
 console.log('Server is running...')
